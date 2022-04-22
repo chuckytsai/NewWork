3ff83e495579e99de38c4d3b374fe3a7c05f9a75
chucky <90169775+ireckchang@users.noreply.github.com>
2022-04-22 14:52:15 +0800

    [龍騰文化一頁網站]
    1. PC 1920px 1280px 切版完成

--- src/pages/LongTeng/LongTeng.js
+++ src/pages/LongTeng/LongTeng.js
@@ -13,6 +13,26 @@ import instruct from "../../images/png/LongTeng/第二屆.png";
 import SignUpIng from "../../images/png/LongTeng/熱烈報名中.png";
 import Group from "../../images/png/LongTeng/Group.png";
 import Protech from "../../images/png/LongTeng/普技高.png";
+import new1 from "../../images/png/LongTeng/new1.png";
+import new2 from "../../images/png/LongTeng/new2.png";
+import new3 from "../../images/png/LongTeng/new3.png";
+import newNews from "../../images/png/LongTeng/最新消息.png";
+import Activity from "../../images/png/LongTeng/活動緣起.png";
+import dot1 from "../../images/png/LongTeng/dot1.png";
+import dot2 from "../../images/png/LongTeng/dot2.png";
+import dot3 from "../../images/png/LongTeng/dot3.png";
+import dot4 from "../../images/png/LongTeng/dot4.png";
+import dot5 from "../../images/png/LongTeng/dot5.png";
+import dot6 from "../../images/png/LongTeng/dot6.png";
+import dot7 from "../../images/png/LongTeng/dot7.png";
+import ActivityImg from "../../images/png/LongTeng/活動緣起圖片1.png";
+import LongTengActivityBackground from "../../images/png/LongTeng/活動時程背景.png";
+import AllSop from "../../images/png/LongTeng/活動流程4步驟.png";
+import Sop1 from "../../images/png/LongTeng/開放報名條件.png";
+import Sop2 from "../../images/png/LongTeng/入圍名單揭曉.png";
+import Sop3 from "../../images/png/LongTeng/得獎名單出爐.png";
+import Sop4 from "../../images/png/LongTeng/成果展.png";
+import LongFooterPC from "../../images/png/LongTeng/龍騰文化頁尾.png";
 
 export function LongTeng() {
     setTimeout(() => { CloseLoanding(); }, 20);
@@ -23,8 +43,7 @@ export function LongTeng() {
         <div className="LongTengGroup" onClick={() => { CloseMenu("all"); }}>
             <nav className="LongTengNav">
                 <div className="LongTengNavText">
-                    <h2 class="LongTengNavTextLogo">龍騰文化 </h2>
-                    {/* <img class="LongTengNavMenu" src="/LongTeng/img/fi_menu.png" alt=""> */}
+                    <h2 className="LongTengNavTextLogo">龍騰文化 </h2>
                     <div className="LongTengNavOptionDiv">
                         <div className="LongTengNavOption">活動辦法</div>
                         <div className="LongTengNavOption">歷屆成果</div>
@@ -55,7 +74,67 @@ export function LongTeng() {
             </div>
             {/* 最新消息群組 */}
             <div className="NewsBlueBackground">
-                
+                <div className="newNewsGroup"><img className="newNews" alt="" src={newNews}></img></div>
+                <div className="ShortlistedNews">
+                    <div className="PinkBackGround"><img className="newImg" alt="" src={new1}></img></div>
+                    <div className="PinkBackGround"><img className="newImg" alt="" src={new2}></img></div>
+                    <div className="PinkBackGround"><img className="newImg" alt="" src={new3}></img></div>
+                </div>
+            </div>
+            {/* 活動緣起 */}
+            <div className="ActivityOrigin">
+                <div className="ActivityOriginHr"><img src={Activity} alt="" /></div>
+                <div className="ActivityOriginCarousel">
+                    <img className="ActivityOriginCarouselImg" src={ActivityImg} alt="" />
+                    <div>
+                        <div className="ActivityOriginCarouselDots">
+                            <img src={dot1} alt="" />
+                            <img src={dot2} alt="" />
+                            <img src={dot3} alt="" />
+                            <img src={dot4} alt="" />
+                            <img src={dot5} alt="" />
+                            <img src={dot6} alt="" />
+                            <img src={dot7} alt="" />
+                        </div>
+                        <h3 className="ActivityOriginCarouselText">
+                            2020年龍騰舉辦了第一屆全國高中職英文素養成果大賞，收到將近500件學生作品，並從中獲得許多感動。我們看見了學生的創意思考與精采表現，更看見了素養的無限可能！龍騰相信，只要給予學生舞台，就有機會「看見‧最美的課堂風景」。
+                            第二屆活動，從英文科擴展到全科；從素養教學延伸到探究實作與自主學習成果。</h3>
+                        <h3 className="ActivityOriginCarouselText">不僅如此，更將「聯合國永續發展目標（SDGs）」的概念融入這次的活動之中。
+                            龍騰相信，好的理念可以被延續，藉由龍騰的拋磚引玉，讓更多創意可以被看見，讓永續目標可以被實踐！</h3>
+                    </div>
+                </div>
+            </div>
+            {/* 活動時程 */}
+            <div className="LongTengActivitySchedule">
+                <div className="LongTengSchedule"><img className="newNews" alt="" src={newNews}></img></div>
+                <div className="LongTengActivityBackground"><img alt="" src={LongTengActivityBackground}></img></div>
+                <div className="PinkPillar">
+                    <div className="PinkPillars"></div>
+                    <div className="PinkPillars"></div>
+                    <div className="PinkPillars"></div>
+                    <div className="PinkPillars"></div>
+                    <div className="PinkPillars"></div>
+                    <div className="PinkPillars"></div>
+                </div>
+                {/* 活動時程排成活動 */}
+                <div className="LongTengActivityScheduleSchedule">
+                    <img className="AllSop" src={AllSop} alt="" />
+                    <div className="ActivityScheduleImgGroup">
+                        <div className="ActivityScheduleImgs ActivityScheduleLeft">
+                            <img className="ActivityScheduleImg" src={Sop2} alt="" />
+                            <img className="ActivityScheduleImg" src={Sop4} alt="" />
+                        </div>
+                        <div className="ActivitySchedulePillars"></div>
+                        <div className="ActivityScheduleImgs ActivityScheduleRight">
+                            <img className="ActivityScheduleImg" src={Sop1} alt="" />
+                            <img className="ActivityScheduleImg" src={Sop3} alt="" />
+                        </div>
+                    </div>
+                </div>
+                {/* 頁尾 */}
+                <footer className="LongFooter">
+                    <img className="LongFooterPC" src={LongFooterPC} alt="" />
+                </footer>
             </div>
         </div>
     </>
