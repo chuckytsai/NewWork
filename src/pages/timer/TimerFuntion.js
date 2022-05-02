b5bdbb37ba3076ba2b8c3fd2c10b04ca3dca6c96
chucky <90169775+ireckchang@users.noreply.github.com>
2022-05-02 21:58:41 +0800

    [修改RWD]
    1.修改RWD

--- src/pages/timer/TimerFuntion.js
+++ src/pages/timer/TimerFuntion.js
@@ -6,8 +6,7 @@ export function FormatData(str) {
     let mn = document.querySelector('#mn');
     let sc = document.querySelector('#sc');
     let newstr = new Date(str);
-    let date = dayjs(newstr).format('YYYY-MM-DD')
-    let Time = dayjs(newstr).format('HH:mm:ss')
+    let Time = dayjs(newstr).format('HH:mm:ss');
     let hh = newstr.getHours() * 30;
     let mm = newstr.getMinutes() * 6;
     let ss = newstr.getSeconds() * 6;
@@ -15,7 +14,7 @@ export function FormatData(str) {
     mn.style.transform = "rotateZ(" + mm + "deg)";
     sc.style.transform = "rotateZ(" + ss + "deg)";
     HelloTimeUp(Time)
-    return `${date} ${Time}`;
+    return `${Time}`;
 };
 
 let Events = String(); //提醒事件陣列
